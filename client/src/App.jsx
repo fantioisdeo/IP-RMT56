9c99579cccc1d3f7a98de7cbaf9a7be5d66c67d5
fantioisdeo <fantioisdeo@gmail.com>
2024-12-31 21:17:07 +0700

    menambahkan tampilan register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route, Outlet } from "react-router";
 import LoginForm from "./pages/Login";
+import RegisterForm from "./pages/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
       </Routes>
     </BrowserRouter>
   );
